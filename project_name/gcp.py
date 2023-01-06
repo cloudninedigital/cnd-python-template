1c4d445db4060bbb3bd53f90a2661ddf81450967
jvvbarbosaCND <joao@cloudninedigital.nl>
2023-01-06 15:29:18 +0100

    Adapt HTTP function to also use functions_framework

--- project_name/gcp.py
+++ project_name/gcp.py
@@ -1,9 +1,9 @@
+import functions_framework
 from project_name.base import export_bucket_file_to_bq, refresh_data_from_api
 
 
 @functions_framework.cloud_event
 def main_cloud_event(cloud_event):
-
     data = cloud_event.data
     bucket = data["bucket"]
     file_name = data["name"]
@@ -15,11 +15,13 @@ def main_cloud_event(cloud_event):
     export_bucket_file_to_bq(file_path)
 
 
+@functions_framework.http
 def main_http_event(request):
-
     try:
         refresh_data_from_api()
     except Exception:
-        return "Processing was unsuccessful"
+        # TODO Improve error reporting
+        return "Processing was unsuccessful", 500
 
-    return "Processing was successful"
+    return "Processing was successful", 200
+
