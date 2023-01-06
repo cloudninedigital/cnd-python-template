f5617c75ecbccffc0ae8332133cfae9c03853a45
jvvbarbosaCND <joao@cloudninedigital.nl>
2023-01-06 17:27:39 +0100

    Add scheduler job to topic

--- terraform/main_pubsub_trigger_gcf.tf
+++ terraform/main_pubsub_trigger_gcf.tf
@@ -12,4 +12,5 @@ EOF
     ORDERS_FOLDER = google_storage_bucket_object.orders_folder.name
     ORDERS_BQ_TABLE = "${google_bigquery_table.orders_table.project}.${google_bigquery_table.orders_table.dataset_id}.${google_bigquery_table.orders_table.table_id}"
   }
+  schedule = "*/2 * * * *"
 }
