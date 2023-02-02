e4dd06018fbc07c0d4faa0e23f944257605096fd
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2023-02-02 09:34:27 +0100

    bigquery executor (without terraform handler so far)

--- project_name/gcp.py
+++ project_name/gcp.py
@@ -23,3 +23,17 @@ def main_http_event(request):
         return "Processing was unsuccessful"
 
     return "Processing was successful"
+
+
+@functions_framework.cloud_event
+def main_bigquery_event(cloud_event):
+
+    data = cloud_event.data
+    bucket = data["bucket"]
+    file_name = data["name"]
+    print(f"Bucket: {bucket}")
+    print(f"File: {file_name}")
+
+    # Implement processing of file here
+    file_path = f"gs://{bucket}/{file_name}"
+    export_bucket_file_to_bq(file_path)
