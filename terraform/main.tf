9d4bfcd8b17a38fa7812ab97fbf62abbe28ca45d
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2023-07-31 14:15:41 +0200

    Made workflows easily deployable through versatile template

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ EOF
   environment = {
     PROJECT           = var.project
     GCS_PROJECT       = var.project
-    GCS_BUCKET_NAME   = var.bucket
+    GCS_BUCKET_NAME   = "bigquery_http_function"
     INCLUDE_VARIABLES = "false"
     SHOW_ALL_ROWS     = "false"
     ON_ERROR_CONTINUE = "false"
@@ -29,14 +29,21 @@ EOF
   }
 }
 
-module "workflows_cf_bigquery_trigger" {
-  source                 = "./modules/workflows_cf_bigquery_trigger"
-  name                   = "workflows-cf-bigquery-test"
-  description            = "a workflow triggered by a table update that calls the bigquery_http_function"
-  project                = var.project
-  dataset                = "some_dataset"
-  table                  = "iets"
-  workflow_template_file = "example.tftpl"
+module "workflows_cf_main_trigger" {
+  source = "./modules/workflows_cf"
+  name = "workflows-cf-bigquery-test-${terraform.workspace}"
+  description = "a workflow triggered by a table update that calls the bigquery_http_function"
+  project = var.project
+  dataset = "some_dataset"
+  table = "iets"
+  trigger_type = "bq"
+  cloudfunctions = [{
+    "name": "bigquery_http_function"
+  },{
+    "name": "bigquery_http_function",
+    "table_updated": "some_dataset.nogiets"
+  }]
+  functions_region = "europe-west1"
 }
 
 
