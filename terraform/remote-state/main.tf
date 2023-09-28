4dbcb279de30cbf132f0c54813898350a584f8fd
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2023-09-28 16:37:28 +0200

    add resourcemanager to remote state

--- terraform/remote-state/main.tf
+++ terraform/remote-state/main.tf
@@ -8,3 +8,9 @@ resource "google_storage_bucket" "tfstate" {
     enabled = true
   }
 }
+
+resource "google_project_service" "cloud_resource_manager" {
+  project            = var.project
+  service            = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
