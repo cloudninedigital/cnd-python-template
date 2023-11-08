c5608bedb403c541bf88a498d7ffaafddddb16d8
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2023-11-08 10:27:04 +0100

    first push

--- project_name/data_quality/main.py
+++ project_name/data_quality/main.py
@@ -1,10 +1,13 @@
 import functions_framework
-from utils import read_configurations
-from cnd_tools.database.bq_data_quality_checker import BQDataQualityChecker
+import pandas as pd
+import os
+from cnd_tools.database.data_quality.bq_data_quality_checker import BQDataQualityChecker
+from cnd_tools.database.bigquery import BigQueryConnection
 
 @functions_framework.cloud_event
 def main_scheduled_data_quality_check(cloud_event):
-    config = read_configurations()
-    check = BQDataQualityChecker(config)
-    check.check_it()
-    return "Processing was successful"
+    dq = BQDataQualityChecker(config_path="./configuration.json")
+    results = dq.check_all_tables(write_results=False)
+    bq = BigQueryConnection(project=os.environ.get("WRITE_PROJECT"))
+    bq.put_data(pd.DataFrame(results),os.environ.get("WRITE_DATASET"), os.environ.get("WRITE_TABLE"), create_if_not_exists=True)
+    return "all checks performed and written"
