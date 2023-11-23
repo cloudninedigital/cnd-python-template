5e18b0529c3cc4995c20c2cfe3ce2e656622af56
Dennis van der Voorn <121879273+dennisCND@users.noreply.github.com>
2023-11-23 07:29:33 +0100

    Update project_name/data_quality/main.py
    Co-authored-by: João Barbosa <101326714+jvvbarbosaCND@users.noreply.github.com>

--- project_name/data_quality/main.py
+++ project_name/data_quality/main.py
@@ -6,7 +6,9 @@ from cnd_tools.database.bigquery import BigQueryConnection
 
 @functions_framework.cloud_event
 def main_scheduled_data_quality_check(cloud_event):
-    dq = BQDataQualityChecker(config_path=f"./{os.environ.get("CONFIGURATION_FILE_NAME")}")
+    from pathlib import Path
+    config_path = Path(__file__).parent / os.environ.get("CONFIGURATION_FILE_NAME", "configuration.json")
+    dq = BQDataQualityChecker(config_path=config_path)
     results = dq.check_all_tables(write_results=False)
     bq = BigQueryConnection(project=os.environ.get("WRITE_PROJECT"))
     bq.put_data(pd.DataFrame(results),os.environ.get("WRITE_DATASET"), os.environ.get("WRITE_TABLE"), create_if_not_exists=True)
