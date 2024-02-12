d5000cf6d023812b14b5167aa480f2ad407df5ac
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2024-02-12 11:08:39 +0100

    extend workflows module with the possibility to execute a dataform pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,33 +2,20 @@
 ### COPY the desired MAIN trigger from ./modules/main_triggers below! 
 ###
 
-# module "gcs_folder_sync" {
-#   source                = "./modules/gcs_folder_sync"
-#   name                  = "${var.application_name}-bqexecutor-sync"
-#   gcs_bucket_file_path  = ""
-#   gcs_local_source_path = "../project_name/SQL/sql_scripts"
-# }
-
-# module "workflows_cf_main_trigger" {
-#   source = "./modules/workflows_cf"
-#   name = "workflows-cf-bigquery-test-${terraform.workspace}"
-#   description = "a workflow triggered by a table update that calls the bigquery_http_function"
-#   project = var.project
-#   dataset = "some_dataset"
-#   table = "iets"
-#   trigger_type = "schedule"
-#   schedule = "*/2 * * * *"
-#   cloudfunctions = [{
-#     "name": "bigquery_http_function",
-#     "table_updated": "some_dataset.iets"
-#   },{
-#     "name": "bigquery_http_function",
-#     "table_updated": "some_dataset.nogiets"
-#   }]
-#   functions_region = "europe-west1"
-#   alert_on_failure = true
-# }
-
+module "workflows_cf_main_trigger" {
+  source = "./modules/workflows"
+  name = "workflows-dataform-test"
+  description = "a workflow triggered by a table update that calls the bigquery_http_function"
+  project = "cloudnine-digital"
+  dataset = "some_dataset"
+  table = "iets"
+  trigger_type = "bq"
+  dataform_pipelines = [{name: "example_dp", tag: "example_tag", repository: "dennis-dataform-test"}]
+  dataform_region = "europe-west3"
+  workflow_type = "dataform"
+  service_account_name = replace("wf-dataform-test", "_", "-")
+  alert_on_failure = true
+}
 
 ###
 ### COPY the desired MAIN trigger from ./modules/main_triggers above!
