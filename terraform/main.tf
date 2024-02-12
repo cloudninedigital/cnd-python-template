5ac24fd1e7d46b4829a759e8097de866117e123d
Dennis van der Voorn <35069808+voornd@users.noreply.github.com>
2024-02-12 11:09:31 +0100

    comment out main implementation

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,20 @@
 ### COPY the desired MAIN trigger from ./modules/main_triggers below! 
 ###
 
-module "workflows_cf_main_trigger" {
-  source = "./modules/workflows"
-  name = "workflows-dataform-test"
-  description = "a workflow triggered by a table update that calls the bigquery_http_function"
-  project = "cloudnine-digital"
-  dataset = "some_dataset"
-  table = "iets"
-  trigger_type = "bq"
-  dataform_pipelines = [{name: "example_dp", tag: "example_tag", repository: "dennis-dataform-test"}]
-  dataform_region = "europe-west3"
-  workflow_type = "dataform"
-  service_account_name = replace("wf-dataform-test", "_", "-")
-  alert_on_failure = true
-}
+# module "workflows_cf_main_trigger" {
+#   source = "./modules/workflows"
+#   name = "workflows-dataform-test"
+#   description = "a workflow triggered by a table update that calls the bigquery_http_function"
+#   project = "cloudnine-digital"
+#   dataset = "some_dataset"
+#   table = "iets"
+#   trigger_type = "bq"
+#   dataform_pipelines = [{name: "example_dp", tag: "example_tag", repository: "dennis-dataform-test"}]
+#   dataform_region = "europe-west3"
+#   workflow_type = "dataform"
+#   service_account_name = replace("wf-dataform-test", "_", "-")
+#   alert_on_failure = true
+# }
 
 ###
 ### COPY the desired MAIN trigger from ./modules/main_triggers above!
--- terraform/modules/main_triggers/main_workflows_trigger.tf
+++ terraform/modules/main_triggers/main_workflows_trigger.tf
@@ -1,18 +1,14 @@
 module "workflows_cf_main_trigger" {
-  source = "./modules/workflows_cf"
-  name = "workflows-cf-bigquery-test-${terraform.workspace}"
+  source = "./modules/workflows"
+  name = "workflows-dataform-test"
   description = "a workflow triggered by a table update that calls the bigquery_http_function"
-  project = var.project
+  project = "cloudnine-digital"
   dataset = "some_dataset"
   table = "iets"
   trigger_type = "bq"
-  cloudfunctions = [{
-    "name": "bigquery_http_function"
-  },{
-    "name": "bigquery_http_function",
-    "table_updated": "some_dataset.nogiets"
-  }]
-  functions_region = "europe-west1"
-  service_account_name = replace("workflows-sa-bq-test-${terraform.workspace}", "_", "-")
+  dataform_pipelines = [{name: "example_dp", tag: "example_tag", repository: "dennis-dataform-test"}]
+  dataform_region = "europe-west3"
+  workflow_type = "dataform"
+  service_account_name = replace("wf-dataform-test", "_", "-")
   alert_on_failure = true
 }
